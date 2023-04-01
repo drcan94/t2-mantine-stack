cb103f3eef7a2c817d355c38498109c77182b9ad
Burak CAN <drcan94@gmail.com>
2023-04-01 17:54:29 +0300

    type problem is fixed

--- src/components/CourseGoals/CourseGoalItem/styles/index.ts
+++ src/components/CourseGoals/CourseGoalItem/styles/index.ts
@@ -1,10 +1,17 @@
 import styled from "@emotion/styled";
+import type { MantineTheme } from "@mantine/core";
 
 export const GoalItem = styled.div`
   margin: 1rem 0;
-  background: ${({ theme }) => theme.colorScheme === 'dark' ? theme.colors.orange[8] : theme.colors.green[8]};
+  background: ${({ theme }: { theme: MantineTheme }) =>
+    theme.colorScheme === "dark"
+      ? theme.colors.orange[8]
+      : theme.colors.green[8]};
   box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
-  color: ${({ theme }) => theme.colorScheme === 'dark' ? theme.colors.yellow[1] : theme.colors.teal[1]};
+  color: ${({ theme }) =>
+    theme.colorScheme === "dark"
+      ? theme.colors.yellow[1]
+      : theme.colors.teal[1]};
   padding: 1rem 2rem;
   cursor: pointer;
 `;
